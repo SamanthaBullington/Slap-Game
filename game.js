51c2b5f266016570e6e04c2f585059670cb98d37
SamanthaBullington <Samantha.J.Bullington@gmail.com>
2021-07-01 17:15:06 -0600

    hit count

--- game.js
+++ game.js
@@ -24,25 +24,72 @@ const castle = {
 
 }
 
-
+let hits = 0
 let health = 100
+
+
+
 //ATTACKS
 
 function treb() {
+  hits++
   health -= attack.trebuchet.damage
   if (health < 0) {
     health = 0
     alert('we been breached')
   }
 
+  document.getElementById('hits').innerText = hits
   document.getElementById('health').innerText = health
 }
+
+function ram() {
+  hits++
+  health -= attack.battering.damage
+  if (health < 0) {
+    health = 0
+    alert('we been breached')
+  }
+
+  document.getElementById('hits').innerText = hits
+  document.getElementById('health').innerText = health
+}
+
+
+function elephant() {
+  hits++
+  health -= attack.eleph.damage
+  if (health < 0) {
+    health = 0
+    alert('we been breached')
+  }
+  document.getElementById('hits').innerText = hits
+  document.getElementById('health').innerText = health
+}
+
+
 
 //DEFENSES
 //INCREMENTS OF 5 STILL GO BELOW ZERO... HOW TO FIX?
 
 function dump() {
   health += castle.oil.defense
+  if (health > 100) {
+    health = 100
+  }
+  document.getElementById('health').innerText = health
+}
+
+function fortify() {
+  health += castle.walls.defense
+  if (health > 100) {
+    health = 100
+  }
+  document.getElementById('health').innerText = health
+}
+
+function shoot() {
+  health += castle.archer.defense
   if (health > 100) {
     health = 100
   }
