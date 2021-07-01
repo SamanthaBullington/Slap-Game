092e249d87babc5b0061da26e5e44ae7a543d575
SamanthaBullington <Samantha.J.Bullington@gmail.com>
2021-07-01 16:03:23 -0600

    js defense dictionary

--- game.js
+++ game.js
@@ -1,15 +1,35 @@
 const attack = {
-  pinch: {
+  trebuchet: {
+    damage: 30,
+  },
+  battering: {
     damage: 10,
+  },
+  eleph: {
+    damage: 20,
   }
+}
+
+const castle = {
+
+  walls: {
+    defense: 50,
+  },
+  oil: {
+    defense: 15,
+  },
+  archer: {
+    defense: 25,
+  }
+
 }
 
 
 let health = 100
 
-function punch() {
+function treb() {
   if (health != 0) {
-    health -= attack.pinch.damage
+    health -= attack.trebuchet.damage
     // } else (alert('he has died'))
 
     document.getElementById('health').innerText = health
