ee70b75aa5a24f93cde484b47f35c87264e59163
SamanthaBullington <Samantha.J.Bullington@gmail.com>
2021-07-01 16:26:38 -0600

    defense function

--- game.js
+++ game.js
@@ -16,22 +16,35 @@ const castle = {
     defense: 50,
   },
   oil: {
-    defense: 15,
+    defense: 20,
   },
   archer: {
-    defense: 25,
+    defense: 10,
   }
 
 }
 
 
 let health = 100
+//ATTACKS
 
 function treb() {
-  if (health != 0) {
-    health -= attack.trebuchet.damage
-    // } else (alert('he has died'))
-
-    document.getElementById('health').innerText = health
+  health -= attack.trebuchet.damage
+  if (health < 0) {
+    health = 0
+    alert('we been breached')
   }
+
+  document.getElementById('health').innerText = health
+}
+
+//DEFENSES
+//INCREMENTS OF 5 STILL GO BELOW ZERO... HOW TO FIX?
+
+function dump() {
+  health += castle.oil.defense
+  if (health > 100) {
+    health = 100
+  }
+  document.getElementById('health').innerText = health
 }
